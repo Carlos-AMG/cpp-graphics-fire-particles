23b4fe3029c5f7d1836a6ce1adeca12dc72f2f00
Carlos Martinez <alberto-cmg@outlook.es>
2021-12-20 11:40:02 -0600

    add better animation, make spiral animation

--- Particle.cpp
+++ Particle.cpp
@@ -3,12 +3,14 @@
 #include <stdlib.h>
 Particle::Particle(): m_ycoord(0), m_xcoord(0){
     m_direction =  (2 * M_PI * rand())/RAND_MAX;
-    m_speed = (0.001 * rand())/RAND_MAX;
+    m_speed = (0.04 * rand())/RAND_MAX;
+    m_speed *= m_speed;
 }
 Particle::~Particle(){
-    
+    //destructor
 }
 void Particle::update_pos(int interval){
+    m_direction += interval * 0.0004;
     double x_speed {m_speed * cos(m_direction)};
     double y_speed {m_speed * sin(m_direction)};
     m_xcoord += x_speed * interval;
